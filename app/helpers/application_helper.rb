8a21793893a860bbbbc7c6f93a853402ed8ebe2e
topfunky <topfunky@7ac60f11-bc0d-0410-ab0a-ac709fd26b10>
2006-03-06 20:58:05 +0000

    Stylesheet Nav fix
    git-svn-id: http://topfunky.net/svn/gullery@15 7ac60f11-bc0d-0410-ab0a-ac709fd26b10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,8 @@ module ApplicationHelper
 
   def show_page_nav
     user = User.find_first
-    nav = link_to((user.company || user.name || 'gullery'), :controller => '/')
+    return if user.nil?
+    nav = link_to(user.company, :controller => '/')
     nav += ' ' + content_tag(:small, link_to((@project.name), projects_url(:action => 'show', :id => @project))) if @project
     nav
   end
